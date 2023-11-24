e128f0138ffd2f51b2870a20a055855cab8dc26e
yorisrisky <151968615+yorisrisky@users.noreply.github.com>
2023-11-25 04:19:20 +0700

    Add files via upload

---
+++ owner-dan-menu.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+
+global.namabot = "Bot Store"
+global.namaowner = "Zeeoneofc"
+global.footer_text = "© " + namabot
+global.pp_bot = fs.readFileSync("./image/foto.jpg")
+global.qris = fs.readFileSync("./image/qris.jpg")
+global.owner = ['62887435047326','6282193115723']
+global.sessionName = 'session'
+global.prefa = ['-_-']
+global.caption_pay = `Qris All Pay
+Ovo
+Dana
+Gopay
+Dan yang lainnya.
+`
+module.exports.helpMenu = (pushname) =>{
+  return `Halo ${pushname}
+
+*「 FITUR BOT 」*
+
+\`\`\`• !owner
+• !pay
+• !list
+• !addlist
+• !updatelist
+• !dellist
+• !jeda
+• !tambah
+• !kurang
+• !kali
+• !bagi
+• !setproses
+• !changeproses
+• !delsetproses
+• !setdone
+• !changedone
+• !delsetdone
+• !proses
+• !done
+• !welcome
+• !goodbye
+• !setwelcome
+• !changewelcome
+• !delsetwelcome
+• !setleft
+• !changeleft
+• !delsetleft
+• !antiwame
+• !antilink
+• !open
+• !close
+• !hidetag
+\`\`\`
+
+📝 *NOTE*: 
+Fitur nya bisa dipakai dengan atau
+tanpa prefix (simbol awalan). Sebagai contoh 
+fitur .owner (prefix)
+dan bisa juga owner (tanpa prefix)
+`
+}
